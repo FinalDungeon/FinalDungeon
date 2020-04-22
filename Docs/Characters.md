70ba1ab6308588af88ad6aae82b8c4c639a53684
Paul Frankovich <pmfrankovich@gmail.com>
2020-04-22 17:38:06 -0400

    PyGame template and Character proposals

---
+++ Docs/Characters.md
@@ -0,0 +1,12 @@
+# Character Proposals
+
+## Allura
+**Race:** Elf\
+**Gender:** Female\
+**Starting Class:** Fighter\
+**Age:** 132\
+**Hieght:** 5'4"\
+**Wieght:** 91 lbs\
+**Hair:** Blonde\
+**Eyes:** Green\
+
---
+++ pygame_template.py
@@ -0,0 +1,44 @@
+# Pygame template - skeleton for a new Pygame project
+import pygame as pg
+import random
+
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+CYAN = (0,255,255)
+MAGENTA = (255,0,255)
+YELLOW = (255,255,0)
+
+# initialize pg and create window
+pg.init()
+pg.mixer.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("My Game")
+clock = pg.time.Clock()
+
+# Game loop
+running = True
+while running:
+    # keep loop running at the right speed
+    clock.tick(FPS)
+    # Process input (events)
+    for event in pg.event.get():
+        # check for closing window
+        if event.type == pg.QUIT:
+            running = False
+
+    # Update
+
+    # Draw / render
+
+    # *after* drawing everything, flip the display
+    pg.display.flip()
+
+pg.quit()
